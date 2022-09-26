64126c76746e4bc4f01ea4a3976dc44eda7794bf
Fagner Moraes <fagm.carvalho@gmail.com>
2022-09-25 23:29:35 -0300

    atualizando projeto

--- api/Program.cs
+++ api/Program.cs
@@ -1,11 +1,24 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add ApplicationDbContext and SQL Server support
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+)
+);
+
+
 
 var app = builder.Build();
 
